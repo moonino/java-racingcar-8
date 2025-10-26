35f0ccecf57c344e525c6f4e8fb861b9b37bc8b3
moonino <dlsgh4090@gmail.com>
2025-10-26 18:56:59 +0900

    feat(validation): 자동차 이름 검증 기능 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,12 +7,15 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class RacingGame {
 
+    private static final int MAX_NAME_LENGTH = 5; // 이름 최대 길이 상수
+
     //게임을 시작하는 메인 메서드
     public void start() {
-        // 1. 입력 기능 구현
+        // 1. 입력 기능
         String carNamesInput = getCarNamesInput();
-        // 2. 유효성 검증 기능 구현
-        validateCarNamesInputFormat(carNamesInput);
+        // 2. 유효성 검증 기능
+        validateCarNamesInputFormat(carNamesInput);//자동차 이름 형식 유효성 검증 기능
+        validateCarNameLengths(carNamesInput);//자동차 이름 길이 유효성 검증 기능
 
         String tryCountInput = getTryCountInput();
 
@@ -39,6 +42,16 @@ public class RacingGame {
     private void validateCarNamesInputFormat(String carNamesInput) {
         if (carNamesInput.endsWith(",")) {
             throw new IllegalArgumentException("자동차 이름은 쉼표(,)로 끝날 수 없습니다.");
+        }
+    }
+
+    //자동차 이름 길이 검증 기능
+    private void validateCarNameLengths(String carNamesInput) {
+        String[] carNames = carNamesInput.split(",");
+        for (String name : carNames) {
+            if (name.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
         }
     }
 }
